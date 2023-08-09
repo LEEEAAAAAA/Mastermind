f83399db2aaa32fe232e06f3740a6091f4eefb85
leaschuerch <lea.schuerch@gmail.com>
2023-08-09 08:33:35 +0200

    generate random colors

--- src/ch.css.mastermind/Main.java
+++ src/ch.css.mastermind/Main.java
@@ -2,7 +2,22 @@ package ch.css.mastermind;
 
 public class Main {
     public static void main(String[] args){
-        
+
+
+        String[] color = {"red", "orange", "yellow", "green", "blue", "purple", "pink", "white"};
+        String[] generatedColors = new String[4];
+        for (int i = 0; i <4 ; i++) {
+            int random = (int) (4*  Math.random());
+            generatedColors[i] = color[random];
+            System.out.println(generatedColors[i]);
+        }
+    }
+
+
+
+
+
+
     }
     
 }
