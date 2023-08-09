6e77ff9d7db64010fc24edfa2210e152928faba8
SaskiaTe <saskia.tellenbach16@gmail.ch>
2023-08-09 09:00:26 +0200

    Color input

--- src/ch.css.mastermind/Main.java
+++ src/ch.css.mastermind/Main.java
@@ -1,5 +1,7 @@
 package ch.css.mastermind;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
 
@@ -12,7 +14,11 @@ public class Main {
             System.out.println(generatedColors[i]);
             System.out.println("Runde " + (i+1));
 
-
+            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Type in color:");
+            x = myObj.nextInt(); // Read user input
+            System.out.println("Type in another color:");
+            y = myObj.nextInt(); // Read user input
 
 
 
