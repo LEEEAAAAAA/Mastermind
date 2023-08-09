40460361c27fad517c4e2b6bd7e16c7cb24971c0
SaskiaTe <saskia.tellenbach16@gmail.ch>
2023-08-09 16:12:35 +0200

    final day 1

--- src/ch.css.mastermind/Main.java
+++ src/ch.css.mastermind/Main.java
@@ -3,27 +3,66 @@ package ch.css.mastermind;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 
         String[] color = {"red", "orange", "yellow", "green", "blue", "purple", "pink", "white"};
+
+
         String[] generatedColors = new String[4];
-        for (int i = 0; i <4 ; i++) {
-            int random = (int) (4*  Math.random());
+        for (int i = 0; i < 4; i++) {
+            int random = (int) (4 * Math.random());
             generatedColors[i] = color[random];
             System.out.println(generatedColors[i]);
-            System.out.println("Runde " + (i+1));
+        }
+        for (int i = 0; i < 12; i++) {
+            System.out.println("Round " + (i + 1));
+
+
+            String[] input = new String[4];
 
             Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-            System.out.println("Type in color:");
-            x = myObj.nextInt(); // Read user input
-            System.out.println("Type in another color:");
-            y = myObj.nextInt(); // Read user input
 
+            for (int currentcolor = 0; currentcolor < 4; currentcolor++) {
+                boolean correctcolor = false;
+                while (!correctcolor) {
+                    System.out.println("Type in " + (currentcolor + 1) + ". color:");
+                    String userInput = myObj.nextLine();
+                    for (int c = 0; c < color.length; c++) {
+                        if (color[c].equals(userInput.toLowerCase())) {
+                            correctcolor = true;
+                        }
+                    }
+                    if (!correctcolor) {
+                        System.out.println("This color does not exist");
 
+                    } else {
+                        input[currentcolor] = userInput;
+                    }
 
+                }
+            }
+
+            int correctcolor = 0, correctplace = 0;
+            for (int j = 0; j < 4; j++){
+                if (input[j].equals(generatedColors[j])) {
+                    correctplace++;
+                }
+                for (int v = 0; v < 4; v++) {
+                if (input[j].equals(generatedColors[v])) {
+                    correctcolor++;
+                }
+
+            }
         }
+
+        System.out.println(correctplace);
+        System.out.println(correctcolor);
+
     }
 
-    
+
 }
+}
+
+
