7177ae4bbd4291b1109f28e6d2d9329ac862f71b
SaskiaTe <saskia.tellenbach16@gmail.ch>
2023-08-10 16:05:44 +0200

    final day 2

--- src/ch.css.mastermind/Main.java
+++ src/ch.css.mastermind/Main.java
@@ -1,64 +1,123 @@
 package ch.css.mastermind;
-
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        boolean startNew = true;
+        while (startNew == true) {
 
 
-        String[] color = {"red", "orange", "yellow", "green", "blue", "purple", "pink", "white"};
+            String[] color = {"red", "orange", "yellow", "green", "blue", "purple", "pink", "white"};
 
 
-        String[] generatedColors = new String[4];
-        for (int i = 0; i < 4; i++) {
-            int random = (int) (4 * Math.random());
-            generatedColors[i] = color[random];
-            System.out.println(generatedColors[i]);
-        }
-        for (int i = 0; i < 12; i++) {
-            System.out.println("Round " + (i + 1));
+            System.out.println("For gameInstruction press i");
+            System.out.println("Press enter to start.");
+            String gameInstruction = myObj.nextLine();
+
+            if ("i".equals(gameInstruction.toLowerCase().trim())) {
+                System.out.println("In this game, there are 8 colors choices: red, orange, yellow, green, blue, purple, pink and white. The Game" +
+                        " generates a random assortment of 4 colors. The games objective is to guess all of the colors in the right order." +
+                        "You have 12 rounds to guess the colors and if you can't guess them correctly in those 12 rounds ... YOU LOOSE! Press enter to start." );
+            } else {
+                System.out.println("Alright then lets start.");
+            }
 
 
-            String[] input = new String[4];
-
-            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-
-            for (int currentcolor = 0; currentcolor < 4; currentcolor++) {
-                boolean correctcolor = false;
-                while (!correctcolor) {
-                    System.out.println("Type in " + (currentcolor + 1) + ". color:");
-                    String userInput = myObj.nextLine();
-                    for (int c = 0; c < color.length; c++) {
-                        if (color[c].equals(userInput.toLowerCase())) {
-                            correctcolor = true;
+            String[] generatedColors = new String[4];
+            for (int r = 0; r < 4; r++) {
+                int samecolors = 2;
+                int randomNumber = 0;
+                while (samecolors >= 2) {
+                    samecolors = 0;
+                    Random random = new Random();
+                    randomNumber = random.nextInt(8);
+                    for (int c = 0; c < 4; c++) {
+                        if (color[randomNumber].equals(generatedColors[c])) {
+                            samecolors++;
                         }
                     }
-                    if (!correctcolor) {
-                        System.out.println("This color does not exist");
+                }
+                generatedColors[r] = color[randomNumber];
+                System.out.println(generatedColors[r]);
+            }
+            for (int i = 0; i < 12; i++) {
+                System.out.println("Round " + (i + 1));
+                int totalCorrectPlaces = 0;
+                int totalWrongPlaces = 0;
 
-                    } else {
-                        input[currentcolor] = userInput;
+                String[] input = new String[4];
+
+
+                for (int currentcolor = 0; currentcolor < 4; currentcolor++) {
+                    boolean correctcolor = false;
+                    while (!correctcolor) {
+                        System.out.println("Type in " + (currentcolor + 1) + ". color:");
+                        String userInput = myObj.nextLine();
+                        for (String s : color) {
+                            if (s.equals(userInput.toLowerCase().trim())) {
+                                correctcolor = true;
+                                break;
+                            }
+                        }
+                        if (!correctcolor) {
+                            System.out.println("This color does not exist");
+
+                        } else {
+                            input[currentcolor] = userInput;
+                        }
+
+                        int wrongplace = 0, correctplace = 0;
+                        String[] colorCopy = {generatedColors[0], generatedColors[1], generatedColors[2], generatedColors[3]};
+                        for (int j = 0; j < 4; j++) {
+
+                            if (colorCopy[j].equals(input[j])) {
+
+                                correctplace++;
+                                colorCopy[j] = "";
+                            }
+                        }
+                        for (int j = 0; j < 4; j++) {
+                            for (int v = 0; v < 4; v++) {
+                                if (colorCopy[v].equals(input[j])) {
+                                    wrongplace++;
+                                    colorCopy[v] = "";
+                                }
+                            }
+                        }
+                        totalWrongPlaces = wrongplace;
+                        totalCorrectPlaces = correctplace;
                     }
+                }
+                System.out.println("Amount of colors in the right place:" + totalCorrectPlaces);
+                System.out.println("Amount of colors that are the right color but in the wrong place:" + totalWrongPlaces);
+
+                if (totalCorrectPlaces == 4) {
+                    System.out.println("You won, congratulations.");
+                    break;
+                }
+                if (i == 11) {
+                    System.out.println("You lost, try better next time.");
 
                 }
-            }
 
-            int correctcolor = 0, correctplace = 0;
-            int v = 0;
-            for (int j = 0; j < 4; j++) {
-                if (input[j].equals(generatedColors[j])) {
-                    correctplace++;
-                } else if (input[j].equals(generatedColors[v])) {
-                    correctcolor++;
-                }
 
             }
+            String playAgain;
+            System.out.println("Would you like to play again? answer with no to exit or press enter to play again.");
+            playAgain = myObj.nextLine();
 
-            System.out.println(correctplace);
-            System.out.println(correctcolor);
+            if ("no".equals(playAgain.toLowerCase().trim())) {
+
+                System.out.println("Ok.");
+                startNew = false;
+                break;
 
 
+            }
         }
     }
 }
+
 
